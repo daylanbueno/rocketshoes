5ada147b0dfefb38cfa5044699567c58d7885ed4
Dailan Bueno <daylansantos@gmail.com>
2020-04-14 22:40:25 -0300

    Ajuste disposicao dos itens

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -65,6 +65,51 @@ export default function Dashborad() {
                     <span>ADICIONAR AO CARRINHO</span>
                 </button>
             </li>
+
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
+
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                    alt="tenis"
+                />
+                <strong>Tênis muito legal</strong>
+                <span>129,90</span>
+                <button type="button">
+                    <div>
+                        <MdAddShoppingCart size={16} color="#fff" /> 3
+                    </div>
+                    <span>ADICIONAR AO CARRINHO</span>
+                </button>
+            </li>
         </ProductList>
     );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,7 +4,7 @@ import { darken } from 'polished';
 
 export const ProductList = styled.ul`
     display: grid;
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(3, 1fr);
     grid-gap: 20px;
     list-style: none;
 
@@ -17,7 +17,7 @@ export const ProductList = styled.ul`
 
         img {
             align-self: center;
-            max-height: 200px;
+            max-height: 250;
         }
 
         > strong {
