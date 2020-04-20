5b52ad09f055032c6363ad5e30ab9260e5c2181e
Dailan Bueno <daylansantos@gmail.com>
2020-04-20 19:26:44 -0300

    Criando component de carrinho

--- src/pages/Card/index.js
+++ src/pages/Card/index.js
@@ -1,7 +1,73 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React from 'react';
-
-import { Container } from './styles';
+import {
+    MdAddCircleOutline,
+    MdRemoveCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Card() {
-    return <Container>Carrinho</Container>;
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th />
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
+                                alt="tenis"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tênis top</strong>
+                            <span>$129,90</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={1} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>$129,90</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar pedito</button>
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>$1920,28</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
