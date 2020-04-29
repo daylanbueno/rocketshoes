b7f42985cf9e961d4680e11667385ebd3900689b
Dailan Bueno <daylansantos@gmail.com>
2020-04-29 18:35:15 -0300

    ajuste para recuperar  produdos de api fake

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,115 +1,45 @@
-import React from 'react';
+/* eslint-disable react/state-in-constructor */
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import { ProductList } from './styles';
 
-export default function Dashborad() {
-    return (
-        <ProductList>
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+class Dashborad extends Component {
+    state = {
+        products: [],
+    };
 
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
+    async componentDidMount() {
+        const response = await api.get('/products');
+        const data = response.data.map((product) => ({
+            ...product,
+            priceFormatted: formatPrice(product.price),
+        }));
+        this.setState({ products: data });
+    }
 
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-
-            <li>
-                <img
-                    src="https://static.netshoes.com.br/produtos/tenis-adidas-lite-racer-cln-masculino/12/COL-7070-012/COL-7070-012_zoom2.jpg?ts=1584624042&ims=326x"
-                    alt="tenis"
-                />
-                <strong>Tênis muito legal</strong>
-                <span>129,90</span>
-                <button type="button">
-                    <div>
-                        <MdAddShoppingCart size={16} color="#fff" /> 3
-                    </div>
-                    <span>ADICIONAR AO CARRINHO</span>
-                </button>
-            </li>
-        </ProductList>
-    );
+    render() {
+        const { products } = this.state;
+        return (
+            <ProductList>
+                {products.map((product) => (
+                    <li key={product.id}>
+                        <img src={product.image} alt={product.title} />
+                        <strong>{product.title}</strong>
+                        <span>{product.priceFormatted}</span>
+                        <button type="button">
+                            <div>
+                                <MdAddShoppingCart size={16} color="#fff" /> 3
+                            </div>
+                            <span>ADICIONAR AO CARRINHO</span>
+                        </button>
+                    </li>
+                ))}
+            </ProductList>
+        );
+    }
 }
+export default Dashborad;
