9d305b0f4cd5dedc2483b909eb01fd32087c17cc
Dailan Bueno <daylansantos@gmail.com>
2020-05-07 09:12:23 -0300

    Validação proptypes Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { MdShoppingBasket } from 'react-icons/md';
@@ -25,6 +26,9 @@ function Header({ cartSize }) {
     );
 }
 
+Header.propTypes = {
+    cartSize: PropTypes.number.isRequired,
+};
 export default connect((state) => ({
     cartSize: state.cart.length,
 }))(Header);
